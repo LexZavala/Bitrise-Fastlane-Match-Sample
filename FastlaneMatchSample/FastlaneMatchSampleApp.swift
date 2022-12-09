3acdfdd3f73d0cfbf15415ad0de565192d79c38d
LexZavala <lex.zavala7@gmail.com>
2022-12-09 15:01:46 -0600

    Initial Commit

---
+++ FastlaneMatchSample/FastlaneMatchSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneMatchSampleApp.swift
+//  FastlaneMatchSample
+//
+//  Created by Alexis Zavala on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneMatchSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
